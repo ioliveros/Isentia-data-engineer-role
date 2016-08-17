886cd0ed16e697c92b3df5af075229e488163ca8
Bernard Wong <bernardwong.work@gmail.com>
2016-08-17 18:55:10 +1000

    feat(pipeline): convert the crawled article content from HTML to plain text(markdown)

--- src/crawler/guardian/pipelines.py
+++ src/crawler/guardian/pipelines.py
@@ -2,23 +2,27 @@ import pymongo
 import configparser
 import json
 from scrapy.exceptions import DropItem
+import html2text
 
 
 class GuardianPipeline(object):
 
     def __init__(self):
+        self.ids_seen = set()
+
+    def open_spider(self, spider):
         config = configparser.ConfigParser()
         config.read('../../config.ini')
         db_host = config.get('mongodb', 'host')
         self.client = pymongo.MongoClient(host=db_host)
         self.db = self.client.get_database('guardian')
-        self.ids_seen = set()
 
     def process_item(self, item, spider):
-        if item['id'] in self.ids_seen:
+        if item['url'] in self.ids_seen:
             raise DropItem("Duplicate item found: %s" % item)
         else:
-            self.ids_seen.add(item['id'])
+            item['content'] = html2text.html2text(item['content'])
+            self.ids_seen.add(item['url'])
             result = self.db.news.insert_one(dict(item))
             return item
 
